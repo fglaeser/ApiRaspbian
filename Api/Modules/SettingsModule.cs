44d68a535dc89ab9c8ce3e345692bccf034b6108
fglaeser <fglaeser@gmail.com>
2018-04-06 22:51:38 -0300

    initial source code commit.

---
+++ Api/Modules/SettingsModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRaspbian.Modules
+{
+  public class SettingsModule : Nancy.NancyModule
+  {
+    public SettingsModule(): base("/api/_settings")
+    {
+      Get("/", (p) => 
+      {
+        return "No Settings yet.";
+      });
+    }
+  }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Infra.Full.Configuration;
+using Infra.WebHost;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace apiraspbian
+{
+    public class Program : IServiceStatus
+  {
+    public ServiceState CurrentState { get; set; }
+
+    public string ServiceName => "ApiRaspbian";
+
+    public string ServiceDisplayName => "ApiRaspbian";
+
+    public string ServiceDescription => "ApiRaspbian";
+
+    public static void Main(string[] args)
+    {
+      //Environment.SetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES", Environment.GetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES") + ";ApiRaspbian");
+      WebHostWrapper.Run(args, new Program());
+    }
+
+    public void AfterStart()
+    {    
+    }
+
+    public void AfterStop()
+    {
+    }
+
+    public void BeforeStart()
+    {
+    }
+
+    public void BeforeStop()
+    {
+    }
+  }
+}

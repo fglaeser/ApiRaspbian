ce4da35ec2b99f861c71a36c8ec74b883c3aab58
fglaeser <fglaeser@gmail.com>
2018-04-09 08:32:48 -0300

    add skeleton reading task.

---
+++ Api/Startup.cs
@@ -0,0 +1,20 @@
+﻿using ApiRaspbian.Tasks;
+using Infra.Full.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ApiRaspbian.Startup))]
+
+namespace ApiRaspbian
+{
+  public class Startup : IHostingStartup
+  { 
+    public void Configure(IWebHostBuilder builder)
+    {
+      builder.ConfigureServices((ctx, c) => {
+        c.AddSingleton<ITaskObject, Reading>();
+        //c.Configure<DataAccessRegistryOptions>(ctx.Configuration.GetSection("DataAccessRegistry"));
+      });
+    }
+  }
+}
---
+++ Api/Tasks/Reading.cs
@@ -0,0 +1,24 @@
+﻿using Infra.Full.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiRaspbian.Tasks
+{
+  public class Reading : ITaskObject
+  {
+    public TimeSpan? WaitTimeout => null;
+
+    public Task StartAsync(CancellationToken token)
+    {
+      while(!token.IsCancellationRequested)
+      {
+        
+      }
+
+      return Task.CompletedTask;
+    }
+  }
+}
